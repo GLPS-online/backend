35974764e0c3354ca6354f219d6facc710c79d2c
vinoankr <vinoankr@gmail.com>
2024-03-26 17:14:42 +0900

    d

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const start = async () => {
         dbName: `glps`,
       }
     );
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(3000, () => console.log("Server started on port 3000"));
   } catch (error) {
     console.log(error);
     // process.exit(1);
