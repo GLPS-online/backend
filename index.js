8ffa498c3c50fda3c261a05c5297d83a48e4488d
vinoankr <vinoankr@gmail.com>
2024-04-27 20:47:28 +0900

    fix

--- index.js
+++ index.js
@@ -11,22 +11,38 @@ const studentRoute = require("./routes/students");
 const userRoute = require("./routes/users");
 const timetableRoute = require("./routes/timetables");
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: true,
-    allowedHeaders: ["Authorization"],
-    preflightContinue: true,
-  })
-);
-app.options(
-  "*",
-  cors({
-    origin: true,
-    allowedHeaders: ["Authorization"],
-    preflightContinue: true,
-  })
-);
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: true,
+//     allowedHeaders: ["Authorization"],
+//     preflightContinue: true,
+//   })
+// );
+// app.options(
+//   "*",
+//   cors({
+//     origin: true,
+//     allowedHeaders: ["Authorization"],
+//     preflightContinue: true,
+//   })
+// );
+
+function setupCORS(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-type,Accept,X-Access-Token,X-Key,Authorization"
+  );
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+}
+app.all("/*", setupCORS);
+
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "WELCOME TO GLPS ONLINE BACKEND SERVER" });
 });
